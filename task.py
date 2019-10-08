35c0d92ed1317257c403a5a89a40527faddfcf88
Gio at UMCU <github@gpiantoni.com>
2019-10-08 14:32:47 +0200

    read tsv

--- task.py
+++ task.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtGui, QtWidgets, QtCore
 from PyQt5.QtGui import QKeyEvent, QPainter
@@ -5,29 +7,22 @@ from PyQt5.QtCore import Qt
 import sys
 from struct import pack
 
-from numpy import arange, r_, c_, array, ones, where
+from numpy import arange, r_, c_, array, ones, where, genfromtxt, zeros, dtype
 from pathlib import Path
 from serial import Serial
 
-image_dir = Path('images/prf').resolve()
-onsets = arange(224) * .85
-offsets = onsets + .5
-onsets *= 1000
-offsets *= 1000
-val = ones((224 * 2, 2))
-val[::2, 0] = onsets
-val[::2, 1] = arange(224).astype(int)
-val[1::2, 0] = offsets
-val[1::2, 1] = 223
+IMAGES_DIR = Path('images/prf').resolve()
+STIMULI_TSV = '/home/giovanni/tools/tasks/images/template_task-prf.tsv'
 
-COM_PORT = 'COM6'
-
+COM_PORT = 'COM9'
+BAUDRATE = 9600
 
 class PrettyWidget(QtWidgets.QLabel):
     started = False
     timer = None
     serial = None
     current_index = 0
+    current_pixmap = None
 
     def __init__(self):
         super().__init__()
@@ -36,11 +31,11 @@ class PrettyWidget(QtWidgets.QLabel):
     def initUI(self):
 
         print('reading images')
-        images = sorted(image_dir.glob('*.png'))
-        self.pixmaps = [QtGui.QPixmap(str(x)) for x in images]
-        self.current_pixmap = self.pixmaps[-1]
+        self.stimuli = _convert_stimuli()
         print('finished reading')
-        im = self.pixmaps[0].toImage()
+
+        # background color
+        im = self.stimuli['pixmap'][0].toImage()
         self.bg_color = QtGui.QColor(im.pixel(0, 0))
         self.setStyleSheet(f"background-color: {self.bg_color.name()};")
         self.show()
@@ -50,7 +45,7 @@ class PrettyWidget(QtWidgets.QLabel):
         self.setCursor(Qt.BlankCursor)
         self.setGeometry(300, 300, 1000, 1000)
         try:
-            self.serial = Serial(COM_PORT, baudrate=11520)
+            self.serial = Serial(COM_PORT, baudrate=BAUDRATE)
         except:
             print('could not open serial port')
         else:
@@ -63,12 +58,13 @@ class PrettyWidget(QtWidgets.QLabel):
 
         rect_x = event.rect().center().x()
         rect_y = event.rect().center().y()
-        img_w = self.current_pixmap.rect().width()
-        img_h = self.current_pixmap.rect().height()
-        img_origin_x = rect_x - int(img_w / 2)
-        img_origin_y = rect_y - int(img_h / 2)
+        if self.current_pixmap is not None:
+            img_w = self.current_pixmap.rect().width()
+            img_h = self.current_pixmap.rect().height()
+            img_origin_x = rect_x - int(img_w / 2)
+            img_origin_y = rect_y - int(img_h / 2)
+            qp.drawPixmap(img_origin_x, img_origin_y, self.current_pixmap)
 
-        qp.drawPixmap(img_origin_x, img_origin_y, self.current_pixmap)
         self.drawText(event, qp)
         qp.end()
 
@@ -80,17 +76,17 @@ class PrettyWidget(QtWidgets.QLabel):
 
     def check_time(self):
 
-        i_im = where(val >= self.time.elapsed())[0][0]
-        index_image = int(val[i_im,1])
+        index_image = where(self.stimuli['onset'] >= self.time.elapsed())[0][0]
 
         if index_image != self.current_index:
             self.current_index = index_image
-            self.current_pixmap = self.pixmaps[index_image]
+            self.current_pixmap = self.stimuli['pixmap'][index_image]
             self.update()
-            if self.serial is not None:
-                self.serial.write(pack('>B', index_image))
 
-        if self.time.elapsed() > 191000:  # TODO
+            if self.serial is not None:
+                self.serial.write(pack('>B', self.stimuli['trigger'][index_image]))
+
+        if self.time.elapsed() > self.stimuli['onset'][-1]:
             self.stop()
 
     def start(self):
@@ -123,6 +119,32 @@ class PrettyWidget(QtWidgets.QLabel):
 
             elif event.key() == Qt.Key_Escape:
                 self.stop()
+
+
+def _convert_stimuli():
+
+    tsv = genfromtxt(
+        fname=STIMULI_TSV,
+        delimiter='\t',
+        names=True,
+        dtype=None,  # forces it to read strings
+        deletechars='',
+        encoding='utf-8')
+    n_stims = tsv.shape[0] * 2  # onsets and offsets
+
+    stimuli = zeros(n_stims, dtype([
+        ('onset', '<f8'),
+        ('pixmap', 'O'),
+        ('trigger', 'uint8'),
+        ]))
+    stimuli['onset'][::2] = tsv['onset']
+    stimuli['onset'][1::2] = tsv['onset'] + tsv['duration']
+    stimuli['onset'] *= 1000  # s -> ms
+    stimuli['trigger'][::2] = tsv['trial_type']
+    stimuli['pixmap'][::2] = [QtGui.QPixmap(str(IMAGES_DIR / png)) for png in tsv['stim_file']]
+    stimuli['pixmap'][1::2] = stimuli['pixmap'][-2]
+
+    return stimuli
 
 
 if __name__ == '__main__':
