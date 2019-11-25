112c7b822e437b8b193202e8a366b9ba42489a0d
Gio Piantoni <github@gpiantoni.com>
2019-11-25 13:11:08 +0100

    test ending

--- qttasks/presentation.py
+++ qttasks/presentation.py
@@ -18,12 +18,14 @@ from serial import SerialException
 from serial.tools.list_ports import comports
 from datetime import datetime
 
+app = QApplication([])
+
 SCRIPT_DIR = Path(__file__).resolve().parent
 IMAGES_DIR = SCRIPT_DIR / 'images'
 SOUNDS_DIR = SCRIPT_DIR / 'sounds'
 LOG_DIR = SCRIPT_DIR / 'log'
 
-with open(SCRIPT_DIR / 'parameters.json') as f:
+with open(SCRIPT_DIR / 'parameters_short.json') as f:
     P = load(f)
 STIMULI_TSV = str(IMAGES_DIR / P['TASK_TSV'])	
 	
@@ -43,6 +45,7 @@ lg.addHandler(logging.StreamHandler())
 
 class PrettyWidget(QtWidgets.QLabel):
     started = False
+    finished = False
     timer = None
     current_index = None
     paused = False
@@ -85,7 +88,7 @@ class PrettyWidget(QtWidgets.QLabel):
         lg.info('Reading images: finished')
 
         # background color
-        im = self.stimuli['pixmap'][0].toImage()
+        im = self.stimuli['pixmap'][1].toImage()
         self.bg_color = QtGui.QColor(im.pixel(0, 0))
         self.setStyleSheet(f"background-color: {self.bg_color.name()};")
         self.show()
@@ -96,7 +99,7 @@ class PrettyWidget(QtWidgets.QLabel):
         self.timer.timeout.connect(self.check_time)
 
         self.setCursor(Qt.BlankCursor)
-        self.setGeometry(300, 300, 1000, 1000)
+        self.setGeometry(200, 200, 1000, 1000)
         self.serial(250)
 
     def serial(self, trigger):
@@ -120,6 +123,7 @@ class PrettyWidget(QtWidgets.QLabel):
 
         elif self.current_index is None:
             self.draw_text(event, qp, 'READY')
+			
         else:
 
             window_rect = event.rect()
@@ -127,8 +131,17 @@ class PrettyWidget(QtWidgets.QLabel):
             rect_y = window_rect.center().y()
             
             current_pixmap = self.stimuli['pixmap'][self.current_index]
-            if current_pixmap is None:
-                self.draw_text(event, qp, '+')
+            if self.current_index == -1 or isinstance(current_pixmap, str):
+                self.draw_text(event, qp, current_pixmap)
+                
+                if current_pixmap == 'DONE':
+                    if not self.finished:
+                        self.finished = True
+                        self.serial(251)
+                        self.serial(None)
+                        if self.sound['end'] is not None:
+                            self.sound['end'].play()
+
             else:
                 image_rect = current_pixmap.rect()
 
@@ -136,11 +149,11 @@ class PrettyWidget(QtWidgets.QLabel):
                 img_origin_x = rect_x - int(size.width() / 2)
                 img_origin_y = rect_y - int(size.height() / 2)
                 qp.drawPixmap(
-                        img_origin_x,
-                        img_origin_y,
-                        size.width(),
-                        size.height(),
-                        current_pixmap)
+                    img_origin_x,
+                    img_origin_y,
+                    size.width(),
+                    size.height(),
+                    current_pixmap)
 
         self.drawText(event, qp)
 
@@ -194,9 +207,10 @@ class PrettyWidget(QtWidgets.QLabel):
 
     def start(self):
         self.started = True
+        self.current_index = -1
         self.time.start()
         self.timer.start(P['QTIMER_INTERVAL'])
-        self.start_serial_input()
+        # self.start_serial_input()
         if self.sound['start'] is not None:
             self.sound['start'].play()
 
@@ -216,16 +230,10 @@ class PrettyWidget(QtWidgets.QLabel):
     def stop(self):
         lg.info('Stopping task')
 
-        self.serial(251)
-        self.serial(None)
-
-        if self.sound['end'] is not None:
-            self.sound['end'].play()
-
         if self.timer is not None:
             self.timer.stop()
 
-        QApplication.quit()
+        self.close()
 
     def pause(self):
         if not self.paused:
@@ -306,25 +314,29 @@ def _convert_stimuli():
         dtype=None,  # forces it to read strings
         deletechars='',
         encoding='utf-8')
-    n_stims = tsv.shape[0] * 2  # onsets and offsets
+    n_stims = tsv.shape[0] * 2 + 3  # onsets and offsets
 
     stimuli = zeros(n_stims, dtype([
         ('onset', '<f8'),
         ('pixmap', 'O'),
         ('trigger', 'uint8'),
         ]))
-    stimuli['onset'][::2] = tsv['onset']
-    stimuli['onset'][1::2] = tsv['onset'] + tsv['duration']
+    stimuli['onset'][1:-2:2] = tsv['onset']
+    stimuli['onset'][2:-2:2] = tsv['onset'] + tsv['duration']
+    stimuli['onset'][-2] = stimuli['onset'][-3] + P['OUTRO']
+    stimuli['onset'][-1] = stimuli['onset'][-2] + 1
     stimuli['onset'] *= 1000  # s -> ms
-    stimuli['trigger'][::2] = tsv['trial_type']
+    stimuli['trigger'][1:-2:2] = tsv['trial_type']
 
     # read images only once
     stimuli['pixmap'] = None
-    stimuli['pixmap'][::2] = tsv['stim_file']
+    stimuli['pixmap'][1:-2:2] = tsv['stim_file']
     d_images = {png: QtGui.QPixmap(str(IMAGES_DIR / png)) for png in set(tsv['stim_file'])}
     for png, pixmap in d_images.items():
         stimuli['pixmap'][stimuli['pixmap'] == png] = pixmap
-
+    stimuli['pixmap'][::2] = P['BASELINE']
+    stimuli['pixmap'][-2] = 'DONE'
+    
     return stimuli
 
 
@@ -338,10 +350,8 @@ def _warn_about_ports():
 
 
 def main():
-    app = QApplication(sys.argv)
     w = PrettyWidget()
-
-    sys.exit(app.exec_())
+    app.exec()
 
 
 if __name__ == '__main__':
